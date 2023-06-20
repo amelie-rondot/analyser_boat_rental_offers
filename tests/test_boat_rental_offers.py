0b6123e5f98d683e7a22a617f0c301a3b0851241
Amélie Rondot <amelie.rondot@multi.coop>
2023-06-20 18:27:08 +0200

    Refacto: refactoring tests

--- tests/test_boat_rental_offers.py
+++ tests/test_boat_rental_offers.py
@@ -2,15 +2,22 @@ import pytest
 
 from boat_rental_offers import BoatRentalOffer, InvalidImmatriculationNumberBoat
 
+URL = "dummy_url"
+WITH_SKIPPER = True
+WITHOUT_SKIPPER = False
+NUC_NUMBER = "123456"  # NUC boat immatriculation number
+NUP_NUMBER = "TLA12345"  # NUP boat immatriculation number
+INVALID_NUMBER = "invalid_number_123"  # invalid boat immatriculation number
+
 
 def test_get_type_boat():
     test_cases = [
         {
-            "offer": BoatRentalOffer("123456", True, "dummy_url"),
+            "offer": BoatRentalOffer(NUC_NUMBER, WITH_SKIPPER, URL),
             "expected": "NUC",
         },
         {
-            "offer": BoatRentalOffer("TLA12345", True, "dummy_url"),
+            "offer": BoatRentalOffer(NUP_NUMBER, WITH_SKIPPER, URL),
             "expected": "NUP",
         },
     ]
@@ -19,36 +26,34 @@ def test_get_type_boat():
         result = tc["offer"].get_boat_type()
         assert result == tc["expected"]
 
-    offer = BoatRentalOffer("invalid_immatriculation_number_123", True, "dummy_url")
+    offer = BoatRentalOffer(INVALID_NUMBER, WITH_SKIPPER, URL)
     with pytest.raises(InvalidImmatriculationNumberBoat):
         offer.get_boat_type()
 
 
 def test_is_illegal():
-    with_skipper = True
-    url = "dummy_url"
 
     # Test case: the boat is a NUP, and it is rented with a skipper -> illegal offer
-    offer_nup_illegal = BoatRentalOffer("TLA12345", with_skipper, url)
+    offer_nup_illegal = BoatRentalOffer(NUP_NUMBER, WITH_SKIPPER, URL)
     expected = True
     result = offer_nup_illegal.is_illegal()
     assert result == expected
 
     without_skipper = False
     # Test case: the boat is a NUP, and it is rented without a skipper -> legal offer
-    offer_nup_legal = BoatRentalOffer("TLA12345", without_skipper, url)
+    offer_nup_legal = BoatRentalOffer(NUP_NUMBER, without_skipper, URL)
     expected = False
     result = offer_nup_legal.is_illegal()
     assert result == expected
 
     # Test case: the boat is a NUC, and it is rented without a skipper -> illegal offer
-    offer_nuc_illegal = BoatRentalOffer("123456", without_skipper, url)
+    offer_nuc_illegal = BoatRentalOffer(NUC_NUMBER, without_skipper, URL)
     expected = True
     result = offer_nuc_illegal.is_illegal()
     assert result == expected
 
     # Test case: the boat is a NUC, and it is rented with a skipper -> legal offer
-    offer_nup_legal = BoatRentalOffer("123456", with_skipper, url)
+    offer_nup_legal = BoatRentalOffer(NUC_NUMBER, WITH_SKIPPER, URL)
     expected = False
     result = offer_nup_legal.is_illegal()
     assert result == expected
