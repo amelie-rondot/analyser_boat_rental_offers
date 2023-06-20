5039e91e8f56ff62b2ab375b401dbf153ee1f185
Amélie Rondot <amelie.rondot@multi.coop>
2023-06-20 17:37:52 +0200

    Refacto: refactoring test

--- tests/test_boat_rental_offers.py
+++ tests/test_boat_rental_offers.py
@@ -4,16 +4,21 @@ from boat_rental_offers import BoatRentalOffer, InvalidImmatriculationNumberBoat
 
 
 def test_get_type_boat():
-    offer_nuc = BoatRentalOffer("123456", True, "dummy_url")
-    expected_boat_type = "NUC"
-    result = offer_nuc.get_type_boat()
-    assert result == expected_boat_type
+    test_cases = [
+        {
+            "offer": BoatRentalOffer("123456", True, "dummy_url"),
+            "expected": "NUC",
+        },
+        {
+            "offer": BoatRentalOffer("TLA12345", True, "dummy_url"),
+            "expected": "NUP",
+        },
+    ]
 
-    offer_nup = BoatRentalOffer("TLA12345", True, "dummy_url")
-    expected_boat_type = "NUP"
-    result = offer_nup.get_type_boat()
-    assert result == expected_boat_type
+    for tc in test_cases:
+        result = tc["offer"].get_type_boat()
+        assert result == tc["expected"]
 
-    unvalid_immat_number_offer = BoatRentalOffer("invalid_immatriculation_number_123", True, "dummy_url")
+    offer = BoatRentalOffer("invalid_immatriculation_number_123", True, "dummy_url")
     with pytest.raises(InvalidImmatriculationNumberBoat):
-        unvalid_immat_number_offer.get_type_boat()
+        offer.get_type_boat()
