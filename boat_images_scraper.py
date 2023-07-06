a9a6952172ce04e08d7aef26c76aed903abf46b5
Amélie Rondot <amelie.rondot@multi.coop>
2023-07-06 10:42:04 +0200

    Add web scraper for boat images from samboat web offers

--- boat_images_scraper.py
+++ boat_images_scraper.py
@@ -11,19 +11,28 @@ get these images urls :
 
 """
 from scrapy import Spider
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 
 from utils import get_url_from_offers_file
 
 
 class ClickAndBoatOfferImagesSpider(Spider):
 
-    name = "images"
+    name = "click_and_boat_images"
 
     # url of a boat rental offer
+    path_file = "tmp_click_and_boat_offers.json"
     start_urls = [
-        get_url_from_offers_file(),
+        get_url_from_offers_file(path_file),
         #f"https://www.clickandboat.com/location-bateau/marseille/semi-rigide/capelli-tempest600-w2bq5d",
     ]
+
+    custom_settings = {
+        "FEEDS": {
+            "tmp_click_and_boat_images.json": {"format": "json"},
+        },
+    }
 
     def parse(self, response):
         """
@@ -41,3 +50,67 @@ class ClickAndBoatOfferImagesSpider(Spider):
         urls_images = images.split(";")
         for url_image in urls_images:
             yield {"url_image": f"https:{url_image}"}
+
+
+class SamBoatOfferImagesSpider(Spider):
+    name = "sam_boat_images"
+
+    # url of a boat rental offer
+    path_file = "tmp_sam_boat_offers.json"
+    start_urls = [
+        # get_url_from_offers_file(path_file),
+        "https://www.samboat.fr/location-bateau/marseille/bateau-a-moteur/45366?picked_from=list",
+    ]
+
+    custom_settings = {
+        "FEEDS": {
+            "tmp_sam_boat_images.json": {"format": "json"},
+        },
+    }
+
+    def parse(self, response):
+        """
+        Implements native parse method, to yield a list of images urls of a Click&Boat web offer into a
+        temporary json file "images.json"
+        :param response:
+        :return:
+        """
+        # <div id="announcement-gallery-modal"">
+        #   ...
+        #      ...
+        #         <div id="announcement-gallery-modal" ...>
+        #            ...
+        #               <div class="row g-3" ...>
+        #                   <div ...>
+        #                      <a href="dummy_url_main_photo">
+        #                           <img ... src="https://cdn.samboat.fr/announcements/dummy_photo1.jpg" ...>
+        #                       </a>
+        #                   </div>
+        #                   <div ...>
+        #                      <a href="dummy_url_main_photo">
+        #                           <img ... src="https://cdn.samboat.fr/announcements/dummy_photo2.jpg" ...>
+        #                       </a>
+        #                   </div>
+        #                   ...
+        #               </div>
+        #            ...
+        #        </div>
+        #      ...
+        #   ...
+        # </div>
+        all_images_selector = response.css('div#announcement-gallery-modal')
+        all_images_sub_selector = all_images_selector.css('div.row.g-3')
+        all_images = all_images_sub_selector.css('div a::attr("href")')
+        for image in all_images:
+            url_image = image.get()
+            url_images = {"url_image": f"{url_image}"}
+            yield url_images
+
+
+if __name__ == "__main__":
+    settings = get_project_settings()
+    process = CrawlerProcess(settings)
+
+    process.crawl(ClickAndBoatOfferImagesSpider)
+    process.crawl(SamBoatOfferImagesSpider)
+    process.start()
